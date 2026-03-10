ff37b7c09c7a02e0438676c59e7a9427c366243b
daryashmidt <daryabulyha@gmail.com>
2026-03-10 17:52:54 +0300

    convert-boolean

---
+++ 8kyu/convert-boolean-values-to-strings-'Yes'-or-'No'.js
@@ -0,0 +1,14 @@
+// ❓
+// Description:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// // ❗️  
+//     assert.strictEqual(boolToWord(true), 'Yes')
+//     assert.strictEqual(boolToWord(false), 'No')
+
+// ✅
+
+function boolToWord(bool){
+    return bool === true ? 'Yes' : 'No';
+}
+
