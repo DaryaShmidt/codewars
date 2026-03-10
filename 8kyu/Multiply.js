92978dff497c4779038958815ece8e1e63d66ab7
daryashmidt <daryabulyha@gmail.com>
2026-03-10 17:08:38 +0300

    multiply

---
+++ 8kyu/Multiply.js
@@ -0,0 +1,13 @@
+// ❓ Description:
+// This code does not execute properly. Try to figure out why.
+
+// ❗️  assert.strictEqual(multiply(1,1), 1);
+//     assert.strictEqual(multiply(2,1), 2);
+//     assert.strictEqual(multiply(2,2), 4);
+
+// ✅
+
+function multiply (a, b){
+  return a * b;
+}
+
