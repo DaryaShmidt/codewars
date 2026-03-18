fb52dad300254a3c4c4321c8ba955220bc1661f3
daryashmidt <daryabulyha@gmail.com>
2026-03-18 23:54:48 +0300

    even or odd

---
+++ 8kyu/even-or-dd.js
@@ -0,0 +1,16 @@
+// // ❓Description:
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// // ❗️
+//   assert.strictEqual(evenOrOdd(2), "Even");  
+
+//  ✅
+
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return 'Even'
+  }
+  else {
+    return 'Odd'
+  }
+};
