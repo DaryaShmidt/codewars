607bfc0412223d5d47f1208e9501e14d7f9802ec
daryashmidt <daryabulyha@gmail.com>
2026-03-26 23:15:34 +0300

    add-a-property

---
+++ 7kyu/add-a-property-to-an-object.js
@@ -0,0 +1,18 @@
+// // ❓Description:
+// Write a function that adds a named property to an object. It must be possible to set the property to a new value. If the property already exists on the object, and error should be thrown.
+// // ❗️  
+
+
+
+//  ✅
+
+
+function addProperty(obj, prop, value) {
+    if (prop in obj) {
+        throw new Error('The property already exists on the object');
+    } else {
+        obj[prop] = value;
+        return obj;
+    }
+}
+
