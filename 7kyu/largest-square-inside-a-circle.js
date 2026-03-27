d06711155c1ab58f928bc336ffa04ca3b652cfaf
daryashmidt <daryabulyha@gmail.com>
2026-03-27 16:25:11 +0300

    Largest Square Inside A Circle

---
+++ 7kyu/largest-square-inside-a-circle.js
@@ -0,0 +1,12 @@
+// // ❓Description:
+// Determine the area of the largest square that can fit inside a circle with radius r.
+ // ❗️  
+//  assert.strictEqual(areaLargestSquare(5), 50);
+//     assert.strictEqual(areaLargestSquare(7), 98);
+
+//  ✅
+
+function areaLargestSquare(r) {
+  return 2 * r**2 ;
+}
+
