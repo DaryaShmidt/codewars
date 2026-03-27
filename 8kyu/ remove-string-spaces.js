815cecc88c41b5671abe17301c67dab84189788f
daryashmidt <daryabulyha@gmail.com>
2026-03-27 20:11:50 +0300

    Remove String Spaces

---
+++ 8kyu/ remove-string-spaces.js
@@ -0,0 +1,13 @@
+// ❓Description:
+// Write a function that removes the spaces from the string, then return the resultant string.≈
+
+// ❗️  
+// "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+
+//  ✅
+
+function noSpace(x){
+    return x.split(' ').join('');
+}
+
+
