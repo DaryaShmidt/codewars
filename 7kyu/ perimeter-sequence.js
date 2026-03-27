50a7293b2e3c90c2e1df62b00d8fe5f27d234b33
daryashmidt <daryabulyha@gmail.com>
2026-03-27 17:23:02 +0300

    Perimeter sequence

---
+++ 7kyu/ perimeter-sequence.js
@@ -0,0 +1,18 @@
+// // ❓Description:
+// The first three stages of a sequence are shown.
+
+// blocks
+
+// The blocksize is a by a and a ≥ 1.
+
+// What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+
+
+// // ❗️  
+
+// assertEquals(perimeterSequence(1,3), 12)
+
+//  ✅
+function perimeterSequence(a,n) {
+  return 4 * a * n;
+}
