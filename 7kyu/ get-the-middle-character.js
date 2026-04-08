5c2bd5431102d234ddd71aea4bf3cdef07fafae2
daryashmidt <daryabulyha@gmail.com>
2026-04-08 11:51:30 +0300

    Get the Middle Character

---
+++ 7kyu/ get-the-middle-character.js
@@ -0,0 +1,19 @@
+// // ❓Description:
+// You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+
+// If the string's length is odd, return the middle character.
+// If the string's length is even, return the middle 2 characters.
+
+// // ❗️  
+// "test" --> "es"
+// "testing" --> "t"
+// "middle" --> "dd"
+// "A" --> "A"
+
+//  ✅
+function getMiddle(s){
+    if (s.length % 2 === 0) {
+        return s[Math.floor((s.length - 1) / 2)] + s[Math.ceil((s.length - 1) / 2)]
+    }
+    return s[(s.length - 1) / 2];
+}
