e817ead84744e72a8a4e31187d2c55a90a89def2
daryashmidt <daryabulyha@gmail.com>
2026-04-16 23:10:50 +0300

    Keep Hydrated!

---
+++ 8kyu/keep-hydrated.js
@@ -0,0 +1,19 @@
+// // ❓
+
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded down.
+
+// // // ❗️ 
+// time = 3 ----> litres = 1
+
+
+// // ✅
+
+function litres(time) {
+    return Math.floor(time * 0.5);
+}
+
+console.log(litres(3));
