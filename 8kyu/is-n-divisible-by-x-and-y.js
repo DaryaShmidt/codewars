975295826fc5b19500b3fdfce143442b0c39599c
daryashmidt <daryabulyha@gmail.com>
2026-04-22 00:03:03 +0300

    Is n divisible by x and y?

---
+++ 8kyu/is-n-divisible-by-x-and-y.js
@@ -0,0 +1,14 @@
+// // ❓
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+
+
+// // // ❗️ 
+// 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+// 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+
+// // ✅
+function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0
+}
+
+console.log(isDivisible(5, 1, 3));
